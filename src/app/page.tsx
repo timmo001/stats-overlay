b31b3fca1e08725c1bc62c633703592c67eff596
Aidan Timson <aidan@timmo.dev>
2024-07-15 23:29:03 +0100

    Shrink text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -84,10 +84,10 @@ export default function HomePage() {
   return (
     <main className="flex min-h-screen w-full flex-col items-start justify-between">
       <section className="container flex w-full flex-row items-start justify-between gap-12 px-4 py-4">
-        <h2 className="text-2xl font-extrabold tracking-tight">
+        <h2 className="text-xl font-extrabold tracking-tight">
           <span className="text-indigo-800">Stats</span> Overlay
         </h2>
-        <h2 className="text-2xl font-extrabold tracking-tight">
+        <h2 className="text-xl font-extrabold tracking-tight">
           CPU: {cpu}% | RAM: {ram}% | GPU: {gpu}% | FPS: {fps}
         </h2>
       </section>
