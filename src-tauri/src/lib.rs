0ee7add820e8cfa4d40a044318e800e995c87122
Aidan Timson <contact@timmo.xyz>
2024-07-16 14:49:11 +0100

    Cleanup

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -64,92 +64,91 @@ pub fn run() {
             update_settings
         ])
         .setup(|app| {
+            let main_window = app.get_webview_window("main").unwrap();
+
+            // Hide the windows on startup
+            main_window.hide().unwrap();
+            app.get_webview_window("settings").unwrap().hide().unwrap();
+
+            // Setup autostart
+            setup_autostart(app).unwrap();
+
+            // Setup tray menu
+            let separator = PredefinedMenuItem::separator(app)?;
+            let toggle_window =
+                MenuItemBuilder::with_id("toggle_window", "Show/Hide window").build(app)?;
+            let show_settings =
+                MenuItemBuilder::with_id("show_settings", "Open settings").build(app)?;
+            let exit = MenuItemBuilder::with_id("exit", "Exit").build(app)?;
+
+            let menu = MenuBuilder::new(app)
+                .items(&[
+                    &toggle_window,
+                    &separator,
+                    &show_settings,
+                    &separator,
+                    &exit,
+                ])
+                .build()?;
+
+            // Setup tray icon
+            let tray = app.tray_by_id("main").unwrap();
+            tray.set_menu(Some(menu))?;
+            tray.on_menu_event(move |app, event| match event.id().as_ref() {
+                "toggle_window" => {
+                    let _ = app.emit("shortcut-event", "Alt+S");
+                }
+                "show_settings" => {
+                    // Get the settings window
+                    let settings_window = app.get_webview_window("settings").unwrap();
+
+                    // Send the show event to the window
+                    settings_window.emit("show", {}).unwrap();
+
+                    // Open devtools on startup
+                    #[cfg(debug_assertions)] // Only include this code on debug builds
+                    {
+                        settings_window.open_devtools();
+                    };
+
+                    // Focus the window
+                    settings_window.set_focus().unwrap();
+                }
+                "exit" => {
+                    std::process::exit(0);
+                }
+                _ => {}
+            });
+
+            // Open devtools on startup
+            #[cfg(debug_assertions)] // Only include this code on debug builds
             {
-                let main_window = app.get_webview_window("main").unwrap();
+                // Open devtools
+                main_window.open_devtools();
+            };
 
-                // Hide the windows on startup
-                main_window.hide().unwrap();
-                app.get_webview_window("settings").unwrap().hide().unwrap();
-
-                // Setup autostart
-                setup_autostart(app).unwrap();
-
-                // Setup tray menu
-                let separator = PredefinedMenuItem::separator(app)?;
-                let toggle_window =
-                    MenuItemBuilder::with_id("toggle_window", "Show/Hide window").build(app)?;
-                let show_settings =
-                    MenuItemBuilder::with_id("show_settings", "Open settings").build(app)?;
-                let exit = MenuItemBuilder::with_id("exit", "Exit").build(app)?;
-
-                let menu = MenuBuilder::new(app)
-                    .items(&[
-                        &toggle_window,
-                        &separator,
-                        &show_settings,
-                        &separator,
-                        &exit,
-                    ])
-                    .build()?;
-
-                // Setup tray icon
-                let tray = app.tray_by_id("main").unwrap();
-                tray.set_menu(Some(menu))?;
-                tray.on_menu_event(move |app, event| match event.id().as_ref() {
-                    "toggle_window" => {
-                        let _ = app.emit("shortcut-event", "Alt+S");
-                    }
-                    "show_settings" => {
-                        // Get the settings window
-                        let settings_window = app.get_webview_window("settings").unwrap();
-
-                        // Send the show event to the window
-                        settings_window.emit("show", {}).unwrap();
-
-                        // Open devtools on startup
-                        #[cfg(debug_assertions)] // Only include this code on debug builds
-                        {
-                            settings_window.open_devtools();
-                        };
-
-                        // Focus the window
-                        settings_window.set_focus().unwrap();
-                    }
-                    "exit" => {
-                        std::process::exit(0);
-                    }
-                    _ => {}
-                });
-
-                // Open devtools on startup
-                #[cfg(debug_assertions)] // Only include this code on debug builds
-                {
-                    // Open devtools
-                    main_window.open_devtools();
-                };
-
-                // Setup hotkeys
-                app.handle().plugin(
-                    match tauri_plugin_global_shortcut::Builder::new().with_shortcuts(["alt+s"]) {
-                        Ok(builder) => builder
-                            .with_handler(|app, shortcut, event| {
-                                if event.state == ShortcutState::Pressed {
-                                    if shortcut.matches(Modifiers::ALT, Code::KeyS) {
-                                        let _ = app.emit("shortcut-event", "Alt+S");
-                                    }
+            // Setup hotkeys
+            app.handle().plugin(
+                match tauri_plugin_global_shortcut::Builder::new().with_shortcuts(["alt+s"]) {
+                    Ok(builder) => builder
+                        .with_handler(|app, shortcut, event| {
+                            if event.state == ShortcutState::Pressed {
+                                if shortcut.matches(Modifiers::ALT, Code::KeyS) {
+                                    let _ = app.emit("shortcut-event", "Alt+S");
                                 }
-                            })
-                            .build(),
-                        Err(e) => {
-                            // Log and return an empty builder
-                            log::error!("Error registering shortcut: {:?}", e);
-                            tauri_plugin_global_shortcut::Builder::new().build()
-                        }
-                    },
-                )?;
+                            }
+                        })
+                        .build(),
+                    Err(e) => {
+                        // Log and return an empty builder
+                        log::error!("Error registering shortcut: {:?}", e);
+                        tauri_plugin_global_shortcut::Builder::new().build()
+                    }
+                },
+            )?;
 
-                setup_stats_thread(app);
-            }
+            setup_stats_thread(app);
+
             Ok(())
         })
         .run(tauri::generate_context!())
